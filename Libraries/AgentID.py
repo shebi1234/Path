5c08f68a7f065597f7127b58232f8cd2506e13e6
shebi1234 <51910550+shebi1234@users.noreply.github.com>
2019-09-19 13:21:04 +0500

    Update AgentID.py

--- Libraries/AgentID.py
+++ Libraries/AgentID.py
@@ -8,7 +8,7 @@ class AgentID:
         return dir_path
 
     def agentid(self):
-        df = pd.read_excel('../agents.xlsx')
+        df = pd.read_excel('Path/agents.xlsx')
         return df['WA ID']
 
     # def rowscount(self):
